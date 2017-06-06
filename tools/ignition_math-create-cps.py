2ef7150a3b4683a4436a03a1661829740f6db3b1
Francois Budin <francois.budin@kitware.com>
2017-06-06 09:20:27 -0400

    Add bazel install rules for ignition_math and ignition_rndf

---
+++ tools/ignition_math-create-cps.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from cpsutils import read_version_defs
+
+defs = read_version_defs("#define IGNITION_MATH_VERSION_FULL[\s]\"([0-9]+).([0-9]+).([0-9]+)\"")
+
+content = """
+{
+  "Cps-Version": "0.8.0",
+  "Name": "ignition_math",
+  "Description": "Math classes and functions for robot applications",
+  "License": "Apache-2.0",
+  "Version": "%(VERSION_MAJOR)s.%(VERSION_MINOR)s.%(VERSION_PATCH)s",
+  "Default-Components": [ ":ignition_math" ],
+  "Components": {
+    "ignition_math": {
+      "Type": "dylib",
+      "Location": "@prefix@/lib/libignition_math.so",
+      "Includes": [ "@prefix@/include" ]
+    }
+  }
+}
+""" % defs
+
+print(content[1:])
